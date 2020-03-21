2110938d8644f740e5352f826a0be441ff42d139
Calandra-a <acalandra@oakland.edu>
2020-03-21 00:37:11 -0400

    Added pVector and cleaned comments

---
+++ pVector.cpp
@@ -0,0 +1,54 @@
+#include "particle.cpp"
+
+struct node{
+ particle p;
+ node* next;
+};
+
+class pVector { 
+private:
+    node* head;
+    node* tail; 
+    int length;  
+public: 
+    pVector();
+    void push_back(particle);
+    particle at(int);
+    int size();
+}; 
+
+pVector::pVector(){ 
+    head = nullptr;
+    tail = nullptr;
+    length = 0;
+} 
+
+void pVector::push_back(particle p){ 
+    //first object case
+    if (length == 0){
+        node *n = new node{p,nullptr};
+        head = n;
+        tail = n;
+        n = nullptr;
+        length++;
+    }
+    //2nd+ object case
+    else{
+        node *n = new node{p,nullptr};
+        tail->next = n;
+        tail = n;
+        length++;
+    }
+} 
+  
+particle pVector::at(int index){ 
+    node *n = head;
+    for (int i =0; i < index; i++){
+        n = n->next;
+    }
+    return n->p;
+}   
+
+int pVector::size(){ 
+    return length; 
+} 
