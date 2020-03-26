6b00c857fb22b3e645a79c5a28f6a023563ae5b8
Calandra-a <acalandra@oakland.edu>
2020-03-25 21:21:31 -0400

    improvement to measurement model and minor clean up

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@
 int main(){
 	//Used for realtime runtime analysis
 	//auto t1 = std::chrono::high_resolution_clock::now();
-<<<<<<< df69327c9a81a5d103aff1b8339e6892addfd34e
 	pVector startP = genParticles(1000);
 	robot r(100,100,100);
 	plot(startP,r);
@@ -18,14 +17,6 @@ int main(){
 		
 		int c[3] = {1,1,1};
 		startP = mcl(startP,c,r);
-=======
-	pVector startP = genParticles(500);
-	robot r(30,30,60);
-	plot(startP,r);
-	for (int i =0; i <20; i++){
-	int c[3] = {1,1,1};
-	startP = mcl(startP,c,r);
->>>>>>> removed some function calls and cleaned up code
 	}
 	//Used for realtime runtime analysis
     //auto t2 = std::chrono::high_resolution_clock::now();
--- measurement_model.cpp
+++ measurement_model.cpp
@@ -1,7 +1,7 @@
 #include "particle.cpp"
+#include "feature.cpp"
+#include "map.cpp"
 #include "robot.cpp"
-<<<<<<< df69327c9a81a5d103aff1b8339e6892addfd34e
-<<<<<<< c04f93f229655bc9cef1aea8318a619e038cc3c6
 #include "helperFunctions.cpp"
 #include "iostream"
 void measurement_model(particle&,robot, particle);
@@ -30,45 +30,3 @@ else
 }
 newP.setW(weight);
 }
-=======
-#include <math.h>
-void measurement_model(particle&,feature,robot, particle, map);
-=======
-#include "helperFunctions.cpp"
-#include "iostream"
-void measurement_model(particle&,robot, particle);
->>>>>>> removed some function calls and cleaned up code
-int prob(int);
-int prob2(int);
-void measurement_model(particle& newP, robot r, particle prevP){
-int deltaX = abs(r.getX() - prevP.getX());
-int deltaY = abs(r.getY() - prevP.getY());
-int deltaT = abs(r.getY() - prevP.getY());
-int totalDelta = deltaT+deltaX+deltaY;
-int weight=100;
-if(totalDelta >=495){
-	weight = 1;
-}
-else
-{
-	int modifer =0;
-	for(int i =5; i< totalDelta; i+=5 ){
-		weight -= 1+modifer;
-		modifer = weight*.05;
-		if(weight<0){
-			weight =1;
-			break;
-		}
-	}	
-}
-newP.setW(weight);
-}
-<<<<<<< df69327c9a81a5d103aff1b8339e6892addfd34e
-int prob(int delta) {
-	int prob;
-	prob = 100 - (delta / 100); //100% - delta(percentage)
-	return prob;
-}
->>>>>>> added in map feature and mm
-=======
->>>>>>> removed some function calls and cleaned up code
