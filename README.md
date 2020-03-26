486cb9f3b2ae631edc6ef34acc0a7a29153d62bc
Calandra-a <acalandra@oakland.edu>
2020-03-26 18:54:38 -0400

    reworked motion model

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Monte-Carlo-Localization
+----remove line: TO BE MERGED INTO REWORK-----
 MCL implementation for CSI-5610 project
 
 built with cmake version 3.5.1 and C++ 11
--- motion_model.cpp
+++ motion_model.cpp
@@ -1,23 +1,30 @@
 #include "particle.cpp" 
 #include "plots/world.cpp"
 #include <math.h>
+#include <iostream>
 #pragma once
 void motion_model(particle&, int[], particle);
+int sample_normal_distribution(int);
 
 void motion_model(particle& newP, int control[2], particle prevP){
-	 //calculations
-	 //control is a 3 item array, velocity (v), degrees of turn (omega), time the control lasts(delta t)
-	newP.setX(
-		prevP.getX() - 
-		(control[0]/control[1])*sin(prevP.getT()) +
-		(control[0]/control[1])*sin(prevP.getT() + control[1]*control[2])
-	);
-	newP.setY(
-		prevP.getY() + 
-		(control[0]/control[1])*cos(prevP.getT()) -
-		(control[0]/control[1])*cos(prevP.getT() + control[1]*control[2])
-	);
-	newP.setT(
-		prevP.getT() + (control[1]*control[2])
-	);
+	int v = control[0];
+	int w = control[1];
+	int vPred = control[0] +sample_normal_distribution((v*v)+(w*w));
+	int wPred = control[1] +sample_normal_distribution((v*v)+(w*w));
+	int preturb = sample_normal_distribution((v*v)+(w*w));
+	int xPred = prevP.getX() - ((vPred/wPred)*sin(prevP.getT())) + ((vPred/wPred)*sin(prevP.getT() + wPred));
+	int yPred = prevP.getY() + ((vPred/wPred)*cos(prevP.getT())) - ((vPred/wPred)*cos(prevP.getT() + wPred));
+	int tPred = prevP.getT() + wPred + preturb;
+	newP.setX(xPred);
+	newP.setY(xPred);
+	newP.setT(tPred);
+}
+
+int sample_normal_distribution(int b){
+	int sample = 0;
+	srand(time(0));
+	for (int i = 0; i < 12; i++){
+		sample +=  (rand()%(b+b)-b);
+		std::cout<<"rand"<<sample<<"\n";
+	}
 }
