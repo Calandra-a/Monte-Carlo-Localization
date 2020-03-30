78998b6239c4bd7f3f30a7db0fe5436186874fbe
Calandra-a <acalandra@oakland.edu>
2020-03-30 18:48:51 -0400

    renamed preturn variable

--- motion_model.cpp
+++ motion_model.cpp
@@ -10,10 +10,10 @@ void motion_model(particle& newP, int control[2], particle prevP){
 	int w = control[1];
 	int vPred = control[0] +sample_normal_distribution((v*v)+(w*w));
 	int wPred = control[1] +sample_normal_distribution((v*v)+(w*w));
-	int preturb = sample_normal_distribution((v*v)+(w*w));
+	int rotation = sample_normal_distribution((v*v)+(w*w));
 	int xPred = prevP.getX() - ((vPred/wPred)*sin(prevP.getT())) + ((vPred/wPred)*sin(prevP.getT() + wPred));
 	int yPred = prevP.getY() + ((vPred/wPred)*cos(prevP.getT())) - ((vPred/wPred)*cos(prevP.getT() + wPred));
-	int tPred = prevP.getT() + wPred + preturb;
+	int tPred = prevP.getT() + wPred + rotation;
 	newP.setX(xPred);
 	newP.setY(xPred);
 	newP.setT(tPred);
